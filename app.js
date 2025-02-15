c87407138822bc0651361d44aca44e6c5e6a64af
hi2suresh <hi2suresh@gmail.com>
2025-02-16 09:04:45 +1100

    added router

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadData } from './services/Menu.js';
+import Router from './services/Router.js';
 
 window.app = {};
 app.store = Store;
+app.Router = Router;
 
 // its better to wait for DOM to be ready
 window.addEventListener('DOMContentLoaded', () => {
   loadData();
+  app.Router.init();
 });
---
+++ services/Router.js
@@ -0,0 +1,48 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll('a.navlink').forEach((link) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const url = event.target.getAttribute('href');
+        Router.go(url);
+      });
+    });
+    // Event handler for URL changes
+    window.addEventListener('popstate', (event) => {
+      Router.go(event.state.route, false);
+    });
+    // Go to the initial URL
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Route is ${route}`);
+    if (addToHistory) {
+      history.pushState({ route }, null, route);
+      let pageElement = null;
+      switch (route) {
+        case '/':
+          pageElement = document.createElement('h1');
+          pageElement.textContent = 'Menu';
+          break;
+        case '/order':
+          pageElement = document.createElement('h1');
+          pageElement.textContent = 'Your order';
+          break;
+        default:
+          if (route.startsWith('/product-')) {
+            pageElement = document.createElement('h1');
+            pageElement.textContent = 'Details';
+            const paramId = route.substring(route.indexOf('-') + 1);
+            pageElement.dataset.id = paramId;
+          }
+      }
+      if (pageElement) {
+        const cache = document.querySelector('main');
+        cache.innerHTML = '';
+        cache.appendChild(pageElement);
+      }
+    }
+  },
+};
+
+export default Router;
